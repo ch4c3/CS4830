0ac3ce3456cb53f51c48a7209af8cd53a8c391f3
Candace Hicks <ch4c3@mail.missouri.edu>
2017-09-28 21:44:12 -0500

    Updated index.php

--- Exploration2/jquery/index.php
+++ Exploration2/jquery/index.php
@@ -2,66 +2,55 @@
 <html lang="us">
 <head>
 	<meta charset="utf-8">
-	<title>Candace's Vacation Photos</title>
+	<title>Exploration 2</title>
 	<!-- for font used -->
 	<link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
 	<!-- Jquery UI imports-->
 	<link href="jquery-ui.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Libre+Barcode+39+Text" rel="stylesheet">
+
 	<script src="external/jquery/jquery.js"></script>
 <script src="jquery-ui.min.js"></script>
+
 	<style>
-	body{
-		background-color: #000000;
+body{
+	background-image: url('black.png');
+}
+#resizable { width: 150px;
+	height: 150px;
+	padding: 0.5em;
+	background-color: transparent;
+ }
+ #resizable h2 { text-align: center;
+	 margin: 0; }
+	 .header{
+		 text-align: center;
+		 margin-left: 0 auto;
 
-	}
-	.header{
-
-		text-align: center;
-	}
-	.container{
-		text-align: center;
-	}
-	h1, u{
+	 }
+	 h1, h2{
+		 font-family: 'Libre Barcode 39 Text', cursive;
 		color: white;
-		font-family: 'Indie Flower', cursive;
-
-	}
-
-
+	 }
 	</style>
+	<script>
+ $( function() {
+	 $( "#resizable" ).resizable();
+ } );
+
+ </script>
 </head>
 <body>
+	<div class="wrapper">
 	<div class="header">
-  <h1>Candace's Photo Database </h1>
+	<h1>Candace's Jquery UI Tutorial</h1>
 </div>
-</body>
-	<?php
-	/* file that holds my database info*/
-	  include("CONFIG.php");
-	/* connecting to my database instance */
-	  $mysqli = new mysqli($HOST_NAME,$USERNAME, $PASSWORD, $DATABASE);
-
-	/*error checking if connection was successful*/
-	  if($mysqli->connect_errno){
-		echo "Could not connect to database";
-		exit();
-	}
-
-	/*query to the database */
-	$sql = "SELECT photo_path FROM pictures;";
-	$result = $mysqli->query($sql);
-
-	/* a table of my pictures from the database*/
-	echo "<div class = 'container'>";
-
-	while($row = $result->fetch_array(MYSQLI_NUM)){
-		foreach($row as $data){
-			echo "<div><img src='" . $data . "' class='img-thumbnail' ></div>";
-		}
-
-	}
-	echo "</div>";
-	 ?>
-
+<div class="interactions">
+	<h2>Interactions</h2>
+	<div id="resizable" class="ui-widget-content">
+<h3 class="ui-widget-header">Resizable boxes</h3>
+	</div>
+	<div>
+</div>
 </body>
 </html>
